241d43d4e392fb263fb5a6e96dc8ea984c9e3f09
aliminagar <127906160+aliminagar@users.noreply.github.com>
2025-07-31 08:01:24 -0500

    Fix button positioning with fixed positioning

--- src/App.js
+++ src/App.js
@@ -366,7 +366,7 @@ function App() {
       {/* Main content */}
       <div
         style={{
-          position: "absolute",
+          position: "fixed",
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
